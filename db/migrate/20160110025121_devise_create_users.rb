854dd94ee91a7b0b2671554266e39eb74b478dc1
Ezra Shim <ezrashim@gmail.com>
2016-01-09 22:19:49 -0500

    hound comment addressed

--- db/migrate/20160110025121_devise_create_users.rb
+++ db/migrate/20160110025121_devise_create_users.rb
@@ -5,22 +5,22 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :role, null: false, default: "member"
       t.string :first_name, null: false
       t.string :last_name, null: false
-      t.string :email,              null: false, default: ""
+      t.string :email, null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
+      t.string :reset_password_token
       t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
 
       ## Trackable
-      t.integer  :sign_in_count, default: 0, null: false
+      t.integer :sign_in_count, default: 0, null: false
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
-      t.inet     :current_sign_in_ip
-      t.inet     :last_sign_in_ip
+      t.inet :current_sign_in_ip
+      t.inet :last_sign_in_ip
 
       ## Confirmable
       # t.string   :confirmation_token
@@ -33,11 +33,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    add_index :users, :email, unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
