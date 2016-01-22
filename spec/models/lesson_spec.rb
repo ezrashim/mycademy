30ea5ba014f7bcc2d6db73990da235f27ce29dec
Ezra Shim <ezrashim@gmail.com>
2016-01-22 11:34:45 -0500

    hound comment addressed

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -8,6 +8,6 @@ describe Lesson do
   it { should have_valid(:content).when("A lot of content goes here. A lot. Whatever you want.") }
   it { should_not have_valid(:content).when("", nil) }
   it { should have_valid(:order_no).when(1) }
-  it { should have_valid(:order_no).when("", nil)}
+  it { should have_valid(:order_no).when("", nil) }
 
 end
