7eac98412fb216b8cccc9c4347808f464db168ba
Ezra Shim <ezrashim@gmail.com>
2016-01-23 20:46:39 -0500

    destroy_lesson created

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,6 +3,8 @@ class LessonsController < ApplicationController
 
   def show
     @lesson = Lesson.find(params[:id])
+    @course = Course.find(params[:course_id])
+    @enrollment = Enrollment.find_by(user: current_user, course: @course)
   end
 
   def create
@@ -23,6 +25,18 @@ class LessonsController < ApplicationController
     @course = Course.find(params[:course_id])
     @lesson = Lesson.new
     @enrollment = Enrollment.find_by(user: current_user, course: @course)
+  end
+
+  def destroy
+    @course = Course.find(params[:course_id])
+    @lesson = Lesson.find(params[:id])
+    if @lesson.destroy
+      flash[:notice] = "Buddy, I hope you have a better lesson in mind, cuz you just deleted one heck of a lesson."
+      redirect_to course_path(@course)
+    else
+      flash.now[:notice] = "Hey, you can't delete this stuff. Who are you trying to trick?"
+      render :show
+    end
   end
 
   private
---
+++ spec/features/lessons/destroy_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+feature 'destroy a lesson', %{
+  As a leader,
+  I should be able to destroy a lesson,
+  So nobody can view the lesson
+  } do
+  # ACCEPTANCE CRITERIA
+  # * As a leader of the course, I can delete a lesson
+  # * So that others cannot view.
+  # * If I am a learner, I cannot delete a lesson.
+
+  let!(:leader) { create :user }
+  let!(:learner) { create :user }
+  let!(:user) { create :user }
+  let!(:course) { create :course_with_lessons }
+  let!(:leader_enrollment) { create :enrollment, role: 'leader', user: leader, course: course }
+  let!(:learner_enrollment) { create :enrollment, role: 'learner', user: learner, course: course }
+
+  scenario "leader can delete the lesson" do
+    lesson = course.lessons.first
+    sign_in_as(leader)
+    visit course_lesson_path(course, lesson)
+
+    expect(page).to have_content lesson.title
+    expect(page).to have_content lesson.content
+    click_link 'Delete Lesson'
+
+    expect(page).to_not have_content lesson.title
+    expect(page).to have_content course.title
+    expect(page).to have_content course.description
+    expect(page).to have_content 'Create Lesson'
+  end
+
+  scenario "learner cannot delete the course" do
+    lesson = course.lessons.first
+    sign_in_as learner
+    visit course_lesson_path(course, lesson)
+
+    expect(page).to have_content lesson.title
+    expect(page).to have_content lesson.content
+    expect(page).to_not have_link 'Delete Lesson'
+  end
+
+
+end
