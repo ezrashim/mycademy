644064af5dbf043b897e4b25823caa8bca2c71c1
Ezra Shim <ezrashim@gmail.com>
2016-01-28 07:58:22 -0500

    card-panel hoverable

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,6 +3,6 @@ class Answer < ActiveRecord::Base
   belongs_to :enrollment
 
   validates :answer, presence: true
-  validates :question_id, presence: true
+  validates :question_id, presence: true, uniqueness: { scope: :enrollment_id }
   validates :enrollment_id, presence: true
 end
