2435c06a40506e9bb918b6c5ed39b9c71dae2d93
Ezra Shim <ezrashim@gmail.com>
2016-01-30 22:16:36 -0500

    hound comments

--- app/assets/javascripts/remove_question.js
+++ app/assets/javascripts/remove_question.js
@@ -4,22 +4,22 @@ $(document).ready(function() {
     event.preventDefault();
     var buttonClicked = event.target
     var question = $('.card-panel.hoverable.question')
-    if (window.confirm("really?")) {
+    if (window.confirm('Are you sure?')) {
       ajaxQuestionDelete(buttonClicked);
     }
   });
 });
 
 var ajaxQuestionDelete = function(buttonClicked) {
-  var questionClass = $($(buttonClicked).closest('row')[0]).attr('class')
-  var questionId = questionClass.match(/question-(\d+)/)[1]
+  var questionClass = $($(buttonClicked).closest('row')[0]).attr('class');
+  var questionId = questionClass.match(/question-(\d+)/)[1];
   var request = $.ajax({
     method: 'DELETE',
     url: '/api/v1/questions/' + questionId
-  })
+  });
 
   request.success(function(data) {
     console.log(data.result);
     $($(buttonClicked).closest('.card-panel')[0]).remove();
-  })
-}
+  });
+};
