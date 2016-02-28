2deab828dd21cc87f59e517227ba25e997ffa6fd
Ezra Shim <ezrashim@gmail.com>
2016-02-28 12:19:44 -0500

    leading and learning courses for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @leading = @user.enrollments.where(role: "leader")
+    @learning = @user.enrollments.where(role: "learner")
   end
 end
