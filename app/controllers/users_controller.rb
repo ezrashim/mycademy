e9bb051864d4f7a64c735861d2a78e170743a272
Ezra Shim <ezrashim@gmail.com>
2016-03-06 22:29:25 -0500

    admin page added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,9 @@ class UsersController < ApplicationController
     @leading = @user.enrollments.where(role: "leader")
     @learning = @user.enrollments.where(role: "learner")
   end
+
+  def index
+    @users = User.all
+    @courses = Course.all
+  end
 end
